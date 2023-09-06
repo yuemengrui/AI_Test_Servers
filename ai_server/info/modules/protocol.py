7eb9befa6f93b3fd915b788915d7ebed2ddb3f5d
yuemengrui <1401190335@qq.com>
2023-09-06 16:09:30 +0800

    update

--- ai_server/info/modules/protocol.py
+++ ai_server/info/modules/protocol.py
@@ -1,6 +1,6 @@
 # *_*coding:utf-8 *_*
 # @Author : YueMengRui
-from pydantic import BaseModel, Field, FileUrl
+from pydantic import BaseModel, Field, AnyUrl
 from typing import Dict, Literal
 
 
@@ -12,6 +12,6 @@ class BaseResponse(BaseModel):
 
 class TableRequest(BaseModel):
     image: str = Field(default=None, repr=False, description="图片base64编码，(不包含base64头)")
-    file_url: FileUrl = Field(description="文件URL")
+    file_url: AnyUrl = Field(description="文件URL")
     file_type: Literal["PDF", "IMAGE"] = Field(description="文件类型")
     table_seg_configs: Dict = Field(default=dict(), description="表格分割超参数")
