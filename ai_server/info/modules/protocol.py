223702b5c1294b460c9f8b59dad36125fc666def
yuemengrui <1401190335@qq.com>
2023-09-06 16:13:14 +0800

    update

--- ai_server/info/modules/protocol.py
+++ ai_server/info/modules/protocol.py
@@ -11,7 +11,7 @@ class BaseResponse(BaseModel):
 
 
 class TableRequest(BaseModel):
-    image: str = Field(default=None, repr=False, description="图片base64编码，(不包含base64头)")
+    # image: str = Field(default=None, repr=False, description="图片base64编码，(不包含base64头)")
     file_url: AnyUrl = Field(description="文件URL")
     file_type: Literal["PDF", "IMAGE"] = Field(description="文件类型")
     table_seg_configs: Dict = Field(default=dict(), description="表格分割超参数")
